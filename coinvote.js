09c90d5500347f29c42b4d742b9356469bbe29e6
ineffectualproperty <kelly.m.olson@gmail.com>
2015-08-30 22:21:37 -0700

    more scripts on ejs page

--- coinvote.js
+++ coinvote.js
@@ -34,6 +34,3 @@ app.get('/', function(request, response) {
 	response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Node app is running on port', app.get('port'));
-});
