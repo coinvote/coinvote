f3ba2a36fc4e0b4a7ad731223cc00a94d5bfeefe
ineffectualproperty <kelly.m.olson@gmail.com>
2015-08-30 20:56:39 -0700

    added some express stuff to coinvote.js

--- coinvote.js
+++ coinvote.js
@@ -1,10 +1,9 @@
 //requirements
 var bitcore = require('bitcore');
 var bitcoreMessage = require('bitcore-message');
-//var express = require('express');
+var express = require('express');
 
-//verification function
-
+//verification function using bitcore, bitcore-message, and node
 var address = 'n1ZCYg9YXtB5XCZazLxSmPDa8iwJRZHhGx';
 var signature = 'H/DIn8uA1scAuKLlCx+/9LnAcJtwQQ0PmcPrJUq90aboLv3fH5fFvY+vmbfOSFEtGarznYli6ShPr9RXwY9UrIY=';
 var voteText = "hello, world";
@@ -23,4 +22,10 @@ var verify = function (voteText, address, signature) {
 
 verify(voteText,address, signature);
 
+// express front end stuff
+express.set('views', __dirname + '/views');
+express.set('view engine', 'ejs');
 
+express.get('/', function(request, response) {
+	response.render('pages/index');
+});
