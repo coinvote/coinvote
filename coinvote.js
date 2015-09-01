d0315c95a515617669cad74259ba317c10dc10a0
ineffectualproperty <kelly.m.olson@gmail.com>
2015-08-31 20:05:25 -0700

    url encode bodyparser

--- coinvote.js
+++ coinvote.js
@@ -4,7 +4,7 @@ var bitcoreMessage = require('bitcore-message');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-app.use(bodyParser()); 
+app.use(bodyParser.urlencode()); 
 
 
 //verification function using bitcore, bitcore-message, and node
