935ce4268c5374568a3e36c8b5437624ae1ecf33
ineffectualproperty <kelly.m.olson@gmail.com>
2015-08-31 20:01:16 -0700

    console printing

--- coinvote.js
+++ coinvote.js
@@ -41,6 +41,7 @@ app.post('/verify',function(req,res){
 	var voteText=req.body.voteTextClient;
 	var address=req.body.addressClient;
 	var signature=req.body.signatureClient;
+	console.log(voteText)
 
 
 	var result = verify(voteText, address, signature);
